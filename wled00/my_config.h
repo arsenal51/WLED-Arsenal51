2998433babed5a97be6eab7ae90553a60e609f6b
arsenal51 <arsenalfiftyone@gmail.com>
2026-02-21 19:32:21 -0500

    Build flags updated for chip identity

--- wled00/my_config.h
+++ wled00/my_config.h
@@ -2,3 +2,10 @@
 
 #define WLED_AP_SSID "Orbiter"
 #define WLED_AP_PASS "deepspace"
+#define WLED_VERSION "1.0.0"
+#define WLED_SERVER_DESCRIPTION "Orbiter"
+#define WLED_BUILD_DATE "2026-02-21"
+#define WLED_HOST_NAME "orbiter"
+#define WLED_UI_TITLE "Orbiter"
+#define WLED_BRAND "Orbiter"
+#define WLED_RELEASE_NAME "ESP32"
